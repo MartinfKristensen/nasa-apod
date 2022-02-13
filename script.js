0344019b6527f60a7c0325699451c99315bdfda1
Martin F. Kristensen <80769574+MartinfKristensen@users.noreply.github.com>
2022-02-13 15:27:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function getDate() {
+  var dateTxt = document.getElementById("date").value;
+  fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=rWjnBrZcElC81cOY6VHdKeYldALXow6Kli2irU88&date=" +
+      dateTxt
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      var day = (document.getElementById("day").innerHTML = data.date);
+      var title = (document.getElementById("title").innerHTML = data.title);
+      var img = (document.getElementById("image").src = data.url);
+      var ex = (document.getElementById("ex").innerHTML = data.explanation);
+    });
+}
+
+function getTodayDate() {
+  fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=rWjnBrZcElC81cOY6VHdKeYldALXow6Kli2irU88"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      var day = (document.getElementById("day").innerHTML = data.date);
+      var title = (document.getElementById("title").innerHTML = data.title);
+      var img = (document.getElementById("image").src = data.url);
+      var ex = (document.getElementById("ex").innerHTML = data.explanation);
+    });
+}
